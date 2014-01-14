bd0c272a153c9856c86c07781a847ceadc6d7ae0
Noah Brook <Noah@Noahs-MacBook-Pro.local>
2014-01-13 19:10:52 -0500

    Add Battleship.java

---
+++ Battleship.java
@@ -0,0 +1,24 @@
+/*=======================================================
+ Battleship- Driver and Gameplay file for Battleship
+========================================================*/
+
+import java.io.*;
+import java.util.*;
+
+public class Battleship{
+    //creates AI for easy difficulty of  game
+    private Easy Bob;
+
+    private Medium Rob;
+    
+    private Hard Sob;
+    //creates variable for me to allow for user input
+    private User Me;
+    //if true game is over
+    private boolean gameOver;
+    //determines difficulty of AI...determines which subclass of AI to call
+    private int difficulty;
+
+
+    
+}//end class Battleship
