ea3bdc973328b3b290acc302228eab8a11df3097
noahbrook97 <noahbrook97@gmail.com>
2014-01-14 19:12:19 -0500

    2nd commit

--- Battleship.java
+++ Battleship.java
@@ -6,19 +6,37 @@ import java.io.*;
 import java.util.*;
 
 public class Battleship{
-    //creates AI for easy difficulty of  game
+ 
+/*~~~~~~~~~~~~~~~~INSTANCE VARIABLE~~~~~~~~~~~~~~~~~~~~~~~
+ 
+  constants that determine how many hits specific ships take before destroyed*/ 
+    public final static int CARRIER_CTR = 5;
+    public final static int BATTLESHIP_CTR = 4;
+    public final static int SUBMARINE_CTR = 3;
+    public final static int DESTROYER_CTR = 3;
+    public final static int PATROLBOAT_CTR = 2;
+ //constant that determines if you have sunken all ships and one game
+    public final static int SHIP_CTR = 5;
+ 
+    /*UNCOMMENT IF DOING DIFFICULTY
+    creates AI for varying difficulty
     private Easy Bob;
-
     private Medium Rob;
-    
     private Hard Sob;
-    //creates variable for me to allow for user input
+    */
+
+ //creates variable for me to allow for user input
     private User Me;
-    //if true game is over
+ //if true game is over
     private boolean gameOver;
-    //determines difficulty of AI...determines which subclass of AI to call
+    /*determines difficulty of AI...determines which subclass of AI to call
+      UNCOMMENT IF DOING DIFFICULTY
     private int difficulty;
+    */
 
-
+ //~~~~~~~~DEFAULT CONSTRUCTOR~~~~~~~~~~~~~~~~~~~~~~
+    public Battleship() {
+	gameOver = false;
+	//newGame();
     
 }//end class Battleship
